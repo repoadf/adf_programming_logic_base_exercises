a7680c96106bda0b17e9f26e4c8f5ab2b8f27a67
Rodrigo Rahman <rodrigorahman@academiadoflutter.com.br>
2023-11-03 09:41:27 -0300

    first commit

---
+++ bin/adf_programming_logic_base_exercises.dart
@@ -0,0 +1,5 @@
+import 'package:adf_programming_logic_base_exercises/adf_programming_logic_base_exercises.dart' as adf_programming_logic_base_exercises;
+
+void main(List<String> arguments) {
+  print('Hello world: ${adf_programming_logic_base_exercises.calculate()}!');
+}
---
+++ test/adf_programming_logic_base_exercises_test.dart
@@ -0,0 +1,8 @@
+import 'package:adf_programming_logic_base_exercises/adf_programming_logic_base_exercises.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
